08325eb65900fbdb682c815625c5c5e82277acd3
Leonardo Jofré <leonardo.jofre@gmail.com>
2018-06-28 01:50:53 -0400

    agregar ejemplo con datos simulados

--- R/constrained.mixed.model.mcmc.R
+++ R/constrained.mixed.model.mcmc.R
@@ -5,18 +5,20 @@
 #' @importFrom invgamma rinvgamma
 #' @export
 constrained.mixed.model.mcmc <- function(n.iter, data){
+
+  n.iter <- n.iter + 2000
+
   Y <- data$Y
   X <- data$X
   U <- data$U
 
   m <- ncol(U)/2
   n <- length(Y)
-  data_sim <- matrix(ncol = 10, nrow = n.iter)
-  beta.g <- data$beta
-  eta.g <- data$eta
-  sigma2.g <- data$sigma2
-  tau00_2.g <- data$tau00_2
-  tau11_2.g <- data$tau11_2
+
+  # setear condiciones iniciales necesarias
+  sigma2.g <- 1
+  tau00_2.g <- 1
+  tau11_2.g <- 1
 
   # partimos con prioris no informativas
   a.sigma2.g <- 0
@@ -26,18 +28,24 @@ constrained.mixed.model.mcmc <- function(n.iter, data){
   a.tau11_2.g <- 0
   b.tau11_2.g <- 0
 
-  chain <- matrix(nrow = n.iter, ncol = 1000)
+  # cadenas
+  chain.beta <- matrix(nrow = n.iter, ncol = ncol(X))
+  chain.eta <- matrix(nrow = n.iter, ncol = ncol(U))
+  chain.sigma2 <- matrix(nrow = n.iter, ncol = 1)
+  chain.tau2 <- matrix(nrow = n.iter, ncol = 2)
 
 
+  eta.g <- matrix(ncol = 1, nrow = ncol(U))
+  eta.g[,] <- 0
   for (t in 1:n.iter) {
 
     # una muestra del vector de factores fijos
     sigma2.beta <- solve((1/sigma2.g) * t(X) %*% X)
-
     mu.beta <- sigma2.beta %*% (0 + (1/sigma2.g) * t(X) %*% (Y - U %*% eta.g))
     beta.g <- mvrnorm(mu = mu.beta, Sigma = sigma2.beta)
 
-    chain[t, 1:length(beta.g)] <- beta.g
+    chain.beta[t,] <- beta.g
+
 
     # actualizacion del vector de factores aleatorios
     for (i in 1:m) {
@@ -52,33 +60,29 @@ constrained.mixed.model.mcmc <- function(n.iter, data){
       eta.g[2 * i, 1] <- rnorm(n = 1, mean = mu.etai1, sd = sqrt(s2.etai1))
     }
 
-    chain[t, (length(beta.g) + 1):(length(beta.g) + length(eta.g))] <- c(eta.g)
+    chain.eta[t,] <- c(eta.g)
 
     # variabilidad global
     a.sigma2.g <- n/2
     b.sigma2.g <- (1/2) * t(Y - X %*% beta.g - U %*% eta.g) %*% (Y - X %*% beta.g - U %*% eta.g)
     sigma2.g = rinvgamma(1, shape = a.sigma2.g, rate = b.sigma2.g)
 
-    chain[t, (length(beta.g) + length(eta.g) + 1):(length(beta.g) + length(eta.g) + 2)] <- sigma2.g
+    chain.sigma2[t,] <- sigma2.g
 
     # variabilidad de intercepto aleatorio por factor
     a.tau00_2.g <- m/2
     b.tau00_2.g <- (1/2) * t(eta.g[seq(from = 1, to = m, by = 2)]) %*% (eta.g[seq(from = 1, to = m, by = 2)])
     tau00_2.g = rinvgamma(1, shape = a.tau00_2.g, rate = b.tau00_2.g)
 
-    chain[t, (length(beta.g) + length(eta.g) + 2):(length(beta.g) + length(eta.g) + 3)] <- tau00_2.g
-
     # variabilidad de la pendiente aleatoria por factor
     a.tau11_2.g <- m/2
     b.tau11_2.g <- (1/2) * t(eta.g[seq(from = 2, to = m, by = 2)]) %*% (eta.g[seq(from = 2, to = m, by = 2)])
     tau11_2.g = rinvgamma(1, shape = a.tau11_2.g, rate = b.tau11_2.g)
 
-    chain[t, (length(beta.g) + length(eta.g) + 3):(length(beta.g) + length(eta.g) + 4)] <- tau11_2.g
-  }
+    chain.tau2[t,] <- c(tau00_2.g, tau11_2.g)
+}
 
   # eliminar los primeros 2000 y saltar de 8 en 8
   index <- seq(from = 2000, to = n.iter, by = 8)
-
-  sample <- chain[index, ]
-  sample
+  list(chain.beta = chain.beta, chain.tau2 = chain.tau2, chain.sigma2= chain.sigma2, chain.eta)
 }
--- test/test.1.R
+++ test/test.1.R
@@ -1,8 +1,7 @@
 library(mkmix)
 
-
 data <- data.generation(n.fixed.effects = 2, n.random.effects = 2, n.sample = 500)
-mcmc.sample <- constrained.mixed.model.mcmc(n.iter = 100000,  data = data)
+mcmc.sample <- constrained.mixed.model.mcmc(n.iter = 1000, data = data)
 d <- density(mcmc.sample[, 9])
 plot(d)
 
